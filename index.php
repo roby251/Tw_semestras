9e0979202a76d66e50c98b1bbf2d70d09a32e273
roby251 <106534644+roby251@users.noreply.github.com>
2022-12-05 19:12:28 -0300

    foter e foterdown en index

--- index.php
+++ index.php
@@ -14,25 +14,8 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <header class="header">
-        <div class="contenedor">
-            <div class="barra">
-                <a href="index.php">
-                    <h1 class="logo no-margin centrar-texto">MyMusic</h1>
-                </a>
-                <nav class="navegacion">
-                    <a class="navegacion__enlace">Iniciar Sesión</a>
-                    <a href="agregarMusic.php" class="navegacion__enlace">Subir Música</a>
-                    <a class="navegacion__enlace">Nosotros</a>
-                    <a href="resena.html" class="navegacion__enlace">Reseñas</a>
-                </nav>
-            </div>
-        </div>
-        <div class="header__texto centrar-texto">
-            <h2 class="no-margin">Comparte y escucha música online</h2>
-            <p class="no-margin">Miles de canciones a un solo click</p>
-        </div>
-    </header>
+    <?php include "header.php"; ?>
+
     <div class="contenedor">
         <h3 class="h3 centrar-texto">Bienvenido a MyMusic</h3>
         <main class="inicio contenido-principal">
@@ -74,20 +57,7 @@
             </article>
         </main>
     </div>
-    <footer class="footer">
-        <div class="contenedor">
-            <div class="barra">
-                <a href="index.php">
-                    <h1 class="logo no-margin centrar-texto">MyMusic</h1>
-                </a>
-                <nav class="navegacion">
-                    <a class="navegacion__enlace">Iniciar Sesión</a>
-                    <a href="agregarMusic.php" class="navegacion__enlace">Subir Música</a>
-                    <a class="navegacion__enlace">Nosotros</a>
-                    <a href="resena.html" class="navegacion__enlace">Reseñas</a>
-                </nav>
-            </div>
-        </div>
-    </footer>
+    <?php include "footer.php"; ?>
+
 </body>
 </html>
