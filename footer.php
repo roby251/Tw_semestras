f48845e4dfc5fc751448c6a96e20971ee05295e2
JackDTowers <113446734+JackDTowers@users.noreply.github.com>
2022-12-05 23:08:32 -0300

    unas weas de estilo

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer class="footer">
     <div class="contenedor">
         <div class="barra">
-            <a href="index.php">
+            <a href="index.php" style="text-decoration: none; color: #fff;">
                 <h1 class="logo no-margin centrar-texto">MyMusic</h1>
             </a>
             <nav class="navegacion">
-                <a class="navegacion__enlace">Iniciar Sesión</a>
-                <a href="agregarMusic.php" class="navegacion__enlace">Subir Música</a>
-                <a class="navegacion__enlace">Nosotros</a>
-                <a href="resena.php" class="navegacion__enlace">Reseñas</a>
+                <a class="navegacion__enlace" style="text-decoration: none; color: #fff;">Iniciar Sesión</a>
+                <a href="agregarMusic.php" class="navegacion__enlace" style="text-decoration: none; color: #fff;">Subir Música</a>
+                <a class="navegacion__enlace" style="text-decoration: none; color: #fff;">Nosotros</a>
+                <a href="resena.php" class="navegacion__enlace" style="text-decoration: none; color: #fff;">Reseñas</a>
             </nav>
         </div>
     </div>
--- header.php
+++ header.php
@@ -1,19 +1,19 @@
 <header class="header">
     <div class="contenedor">
         <div class="barra">
-            <a href="index.php">
+            <a style="text-decoration: none; color: #fff;" href="index.php">
                 <h1 class="logo no-margin centrar-texto">MyMusic</h1>
             </a>
             <nav class="navegacion">
-                <a class="navegacion__enlace">Iniciar Sesión</a>
-                <a href="agregarMusic.php" class="navegacion__enlace">Subir Música</a>
-                <a class="navegacion__enlace">Nosotros</a>
-                <a href="resena.php" class="navegacion__enlace">Reseñas</a>
+                <a class="navegacion__enlace" style="text-decoration: none; color: #fff;">Iniciar Sesión</a>
+                <a href="agregarMusic.php" class="navegacion__enlace" style="text-decoration: none; color: #fff;">Subir Música</a>
+                <a class="navegacion__enlace" style="text-decoration: none; color: #fff;">Nosotros</a>
+                <a href="resena.php" class="navegacion__enlace" style="text-decoration: none; color: #fff;">Reseñas</a>
             </nav>
         </div>
     </div>
     <div class="header__texto centrar-texto">
         <h2 class="no-margin">Comparte y escucha música online</h2>
-        <p class="no-margin">Miles de canciones a un solo click</p>
+        <p class="no-margin" style="font-size: 1.2rem; line-height: 1.8;">Miles de canciones a un solo click</p>
     </div>
 </header>
